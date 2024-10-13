5051dbf42754e2c2d27e8cfde5ea99a254dfaaef
Thiago Erik <thiagoerik55@gmail.com>
2024-10-13 19:50:09 -0300

    Commit teste

--- ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4345c39de95d244b2e9503965c4b2f6195937d8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2165a57c7f457de7d40696624517facb2538d792")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
